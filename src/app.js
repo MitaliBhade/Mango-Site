3969008821c912b068461d2b8ad9f1c5a4166a4a
Mitali Bhade <63578995+undefined@users.noreply.github.com>
2022-04-30 11:33:12 +0000

    Commit-1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,38 @@
 import { LightningElement } from "lwc";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class App extends LightningElement {
-  title = "Welcome to Lightning Web Components!";
-
+  title = "Orders Mangoes for the season now!!!";
+  details;
+  details1;
+  details2;
+  details3;
+  disableButton = false;
+  ready = false;
+ 
+  
   showFeatures = true;
+
+  loadDetails(event) {
+    this.ready = true;
+    this.details = "Cost per dozen";
+    this.details1 = "Small Size 700rs";
+    this.details2 = "Medium Size 800rs";
+    this.details3 = "Large Size 900rs";
+    this.disableButton = true;
+    
+     
+  }
+
+  showSuccessToast() {
+        const evt = new ShowToastEvent({
+            title: 'Toast Success',
+            message: 'Opearion sucessful',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
+    }
 
   /**
    * Getter for the features property
